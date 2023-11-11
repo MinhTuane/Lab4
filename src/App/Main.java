901570874ddd43a5a7bcbb2bc0efd5758cd1f572
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2023-11-11 15:08:32 +0700

    update

---
+++ src/App/Main.java
@@ -0,0 +1,11 @@
+package App;
+
+import Model.Tenant;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tenant tenant = new Tenant(0, "Tuan", "La", "dev@gmai.com", "pass", null);
+	}
+
+}
