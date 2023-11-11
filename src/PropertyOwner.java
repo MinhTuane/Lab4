d33c3cacc8c199ab05559bf0bf98b519874543e1
MinhTuane <tuan.pham.set18@eiu.edu.vn>
2023-11-11 15:15:11 +0700

    v1_11/11/2023

--- src/PropertyOwner.java
+++ src/PropertyOwner.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class PropertyOwner {
-
+public class PropertyOwner extends User{
+	public PropertyOwner(String UserID, String firsName, String lastName, String email, String passWord) {
+		super(UserID, firsName, lastName, email, passWord);
+		// TODO Auto-generated constructor stub
+		this.OwnerID= UserID;
+	}
+	public Scanner sc = new Scanner(System.in);
+	User user;
+	String OwnerID;
+	public List<User> listProperty= new ArrayList<>();
+	public List<String> listOwner= new ArrayList<>();
+	@Override
+	public void register() {
+		this.sysout("ID :");
+		String User = this.InputString();
+		this.sysout("First Name");
+		String firsNam= this.InputString();
+		this.sysout("Last Name: ");
+		String lastNam= this.InputString();
+		this.sysout("Email: ");
+		String emai= this.InputString();
+		this.sysout("Password: ");
+		String passWor= this.InputString();
+		User use = new PropertyOwner(User,firsNam,lastNam,emai,passWor);
+		this.listUser.add( use );
+	}
+	
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class User {
-
+public abstract class User {
+	String UserID;
+	String firstName;
+	String lastName;
+	String Email;
+	String password;
+	PropertyOwner po;
+	public Scanner sc= new Scanner(System.in);
+	List<User> listUser= new ArrayList<>();
+	
+	public User(String UserID,String firsName,String lastName,String email, String passWord){
+		this.UserID=UserID;
+		this.firstName=firsName;
+		this.lastName= lastName;
+		this.Email= email;
+		this.password=passWord;
+		
+	}
+	public void register() {
+		
+		
+	}
+	public boolean login() {
+		this.sysout("Email");
+		 String tEmail=  this.InputString();
+		this.sysout("Password");
+		String tPassWord= this.InputString();
+		if(findUser(tEmail,tPassWord)==-1)
+			return false;
+		return true;
+	}
+	public boolean update() {
+		this.sysout("ID want to change");
+		String id= this.InputString();
+		int userPosition = this.findUserID(id);
+		if(userPosition==-1) {
+			return false;
+		}else {
+			this.listUser.remove(userPosition);
+			po.register();
+			return true;
+		}
+	}
+	public String InputString() {
+		return sc.next();
+	}
+	public void sysout(String a) {
+		System.out.println("a");
+	}
+	public int findUser(String tEmail,String tPassWord) {
+		
+		int i=0;
+		for(User a:this.listUser) {
+			if(a.Email.equalsIgnoreCase(tEmail)&& a.password.equals(tPassWord));
+			 	return i;
+		}
+		return -1;
+	}
+	public boolean resetPassword() {
+		this.sysout("ID want to change");
+		String id= this.InputString();
+		int userPosition = this.findUserID(id);
+		if(userPosition==-1) {
+			return false;
+		}else {
+			this.sysout("New Password:");
+			this.setPassword(this.InputString());
+			return true;
+		}
+	}
+	public int findUserID(String id) {
+		
+		int i=0;
+		for(User a:this.listUser) {
+			if(a.UserID.equalsIgnoreCase(id));
+			 	return i;
+		}
+		return -1;
+	}
+	public void setPassword(String a) {
+		this.password= a;
+	}
 }
